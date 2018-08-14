9bbbd9e1149e737813f59765de77c55c57e99ddf
NoemiMolnar <noemi.m.molnar@gmail.com>
2018-08-14 17:55:15 +0200

    makeMove function for snakes

--- client/src/components/games/GameDetails.js
+++ client/src/components/games/GameDetails.js
@@ -1,9 +1,9 @@
-import React, {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
-import {getGames, joinGame, updateGame} from '../../actions/games'
-import {getUsers} from '../../actions/users'
-import {userId} from '../../jwt'
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { getGames, joinGame, updateGame } from '../../actions/games'
+import { getUsers } from '../../actions/users'
+import { userId } from '../../jwt'
 import Paper from 'material-ui/Paper'
 import Board from './Board'
 import './GameDetails.css'
@@ -14,31 +14,83 @@ class GameDetails extends PureComponent {
     if (this.props.authenticated) {
       if (this.props.game === null) this.props.getGames()
       if (this.props.users === null) this.props.getUsers()
+
     }
   }
 
   joinGame = () => this.props.joinGame(this.props.game.id)
 
+  // makeMove = (toRow, toCell) => {
+  //   const {game, updateGame} = this.props
+
+  //   const board = game.board.map(
+  //     (row, rowIndex) => row.map((cell, cellIndex) => {
+  //       if (rowIndex === toRow && cellIndex === toCell) return game.turn
+  //       else return cell
+  //     })
+  //   )
+  //   updateGame(game.id, board)
+  // }
+
+
   makeMove = (toRow, toCell) => {
-    const {game, updateGame} = this.props
+    const snake = [[2, 2]]
+    snake.unshift([toRow,toCell])
+    snake.pop()
+    const { game, updateGame } = this.props
+    // let board = game.board.map(
+    //   (row, rowIndex) => row.map((cell, cellIndex) => {
+    //     if (rowIndex === toRow && cellIndex === toCell) {
+    //       snake.map((part) => {
+    //         if (rowIndex === part[0] && cellIndex === part[1]) {
+    //           console.log(game.turn)
+    //         }
+    //       })
+    //       return game.turn
+    //       // return null
+    //     }
+    //     else {
+    //       // return  snake.map((part) => {
+    //       //     if (rowIndex === part[0] && cellIndex === part[1]) {
+    //       //       console.log(game.turn)
+    //       //       return cell
+    //       //     } else {
+    //       return cell
+    //     }
+    //   })
+    // )
 
-    const board = game.board.map(
-      (row, rowIndex) => row.map((cell, cellIndex) => {
-        if (rowIndex === toRow && cellIndex === toCell) return game.turn
-        else return cell
-      })
-    )
+
+   console.log(snake)
+
+  const board = game.board.map(
+    (row, rowIndex) => row.map((cell, cellIndex) => {
+     let rs = snake.map((part) => {
+        if (rowIndex === part[0] && cellIndex === part[1]) {
+          return game.turn
+        }
+        else {
+          return cell
+        }
+      }
+      ) 
+      return rs[0]
+
+    }
+    ))
+
     updateGame(game.id, board)
+    //update snake
   }
-
+  
 
 
   render() {
-    const {game, users, authenticated, userId} = this.props
 
+    const { game, users, authenticated, userId } = this.props
     if (!authenticated) return (
-			<Redirect to="/login" />
-		)
+      <Redirect to="/login" />
+    )
 
     if (game === null || users === null) return 'Loading...'
     if (!game) return 'Not found'
