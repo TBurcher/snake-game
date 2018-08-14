a4f4af6dab23b20aa8d9d5c7aafd240dcb02e568
“TOM <YOUR@EMAIL.com>
2018-08-14 12:33:14 +0200

    initial set up from tic-tac-toe

---
+++ client/src/reducers/index.js
@@ -0,0 +1,13 @@
+import games from './games'
+import users from './users'
+import login from './login'
+import currentUser from './currentUser'
+import signup from './signup'
+
+export default {
+  signup,
+  login,
+  currentUser,
+  users,
+  games,
+}
---
+++ client/src/store.js
@@ -0,0 +1,25 @@
+import {createStore, applyMiddleware, combineReducers, compose} from 'redux'
+import ReduxThunk from 'redux-thunk'
+import reducers from './reducers'
+import {storeJwt, socketIo} from './middleware'
+import SocketIO from './socketio'
+
+const reducer = combineReducers(reducers)
+
+const devTools = window.devToolsExtension ? window.devToolsExtension() : f => f
+
+const socket = new SocketIO()
+
+const enhancer = compose(
+	applyMiddleware(ReduxThunk, storeJwt, socketIo(socket)),
+	devTools
+)
+
+const store = createStore(reducer, enhancer)
+
+const initialCurrentUser = store.getState().currentUser
+if (initialCurrentUser) {
+  socket.connect(store.dispatch, initialCurrentUser.jwt)
+}
+
+export default store
