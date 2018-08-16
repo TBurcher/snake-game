d0ba49a90979594b1b006260b7ecd8143a8e68ab
“TOM <YOUR@EMAIL.com>
2018-08-16 15:39:33 +0200

    some board icons and top banner

--- client/src/components/games/Board.js
+++ client/src/components/games/Board.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Board.css'
+import mouse from '../icons/mouse.svg'
+import grass from '../icons/grass.svg'
 
 const renderCel = (rowIndex, cellIndex, symbol, hasTurn) => {
   return (
@@ -7,7 +9,7 @@ const renderCel = (rowIndex, cellIndex, symbol, hasTurn) => {
       className="board-tile"
       disabled={hasTurn}
       key={`${rowIndex}-${cellIndex}`}
-    >{symbol || '.' }</button>
+    >{symbol || <img src={grass} alt="grass" /> }</button>
   )
 }
 
@@ -15,7 +17,7 @@ export default ({board, coin}) => board.map((cells, rowIndex) =>
   <div key={rowIndex}>
     {cells.map((symbol, cellIndex) => {
       if (rowIndex === coin[0] && cellIndex === coin[1]) {
-        return renderCel(rowIndex, cellIndex, '$', false)
+        return renderCel(rowIndex, cellIndex, <img src={mouse} alt="mouse" />, false)
       } 
       else { return renderCel(rowIndex, cellIndex, symbol, false) }
     })}
