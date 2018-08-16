9f5b9b1f65cca09da9aa6b1766862aff35665f28
NoemiMolnar <noemi.m.molnar@gmail.com>
2018-08-16 15:40:40 +0200

    for merging

--- client/src/components/games/Board.js
+++ client/src/components/games/Board.js
@@ -7,16 +7,25 @@ const renderCel = (rowIndex, cellIndex, symbol, hasTurn) => {
       className="board-tile"
       disabled={hasTurn}
       key={`${rowIndex}-${cellIndex}`}
-    >{symbol || '.' }</button>
+    >{symbol || '.'}</button>
   )
 }
 
-export default ({board, coin}) => board.map((cells, rowIndex) =>
+export default ({ board, coin }) => board.map((cells, rowIndex) =>
   <div key={rowIndex}>
     {cells.map((symbol, cellIndex) => {
       if (rowIndex === coin[0] && cellIndex === coin[1]) {
         return renderCel(rowIndex, cellIndex, '$', false)
-      } 
+      } else if (symbol === 'x') {
+        return renderCel(rowIndex, cellIndex,
+          // <svg>
+          //   <circle cx="5" cy="5" r="4"  fill="red" />
+          // </svg> 
+          'x'
+          , false)
+      } else if (symbol === 'o') {
+        return renderCel(rowIndex, cellIndex, 'o', false)
+      }
       else { return renderCel(rowIndex, cellIndex, symbol, false) }
     })}
   </div>
