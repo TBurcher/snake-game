4e6612bdc22fb71f5d0c39c40bb0e1f68d1ea138
“TOM <YOUR@EMAIL.com>
2018-08-16 17:21:45 +0200

    its broken

--- client/src/components/games/Board.js
+++ client/src/components/games/Board.js
@@ -18,19 +18,7 @@ export default ({ board, coin }) => board.map((cells, rowIndex) =>
     {cells.map((symbol, cellIndex) => {
       if (rowIndex === coin[0] && cellIndex === coin[1]) {
         return renderCel(rowIndex, cellIndex, <img src={mouse} alt="mouse" />, false)
-
-      } else if (symbol === 'x') {
-        return renderCel(rowIndex, cellIndex,
-          // <svg>
-          //   <circle cx="5" cy="5" r="4"  fill="red" />
-          // </svg> 
-          'x'
-          , false)
-      } else if (symbol === 'o') {
-        return renderCel(rowIndex, cellIndex, 'o', false)
       }
-
-
       else { return renderCel(rowIndex, cellIndex, symbol, false) }
     })}
   </div>
