1f26c3de6d0131a1f7a52c701209c572e98dfd09
NoemiMolnar <noemi.m.molnar@gmail.com>
2018-08-17 12:31:32 +0200

    board header style

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 export const baseUrl = 'http://localhost:4000'
 //Noemi 
-//export const baseUrl = 'http://172.16.29.16:4000'
+// export const baseUrl = 'http://172.16.29.16:4000'
 //Tom
 //export const baseUrl = 'http://172.16.30.220:4000'
 
